1db405baf0822c37df03c6c5e9561c35531b4f72
Dmitry Kochelyagin <taynov.pro@outlook.com>
2026-06-14 20:16:06 +0300

    Initial project setup

---
+++ src/main/java/ru/onequ1z/playtime_tracker/PlayTimeTrackerMod.java
@@ -0,0 +1,47 @@
+package ru.onequ1z.playtime_tracker;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(PlayTimeTrackerMod.MOD_ID)
+public class PlayTimeTrackerMod {
+    public static final String MOD_ID = "playtime_tracker";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public PlayTimeTrackerMod(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+
+        NeoForge.EVENT_BUS.register(this);
+
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
